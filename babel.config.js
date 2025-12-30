b7b2bccf7e62f491097bb4e3480fb3f2e1921f2e
Ilesanmi Tolulope <marvellousilesanmi25@gmail.com>
2025-12-30 18:08:31 +0100

    fixing installation issue

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'expo-router/babel',
+      // Reanimated plugin must be listed last
+      'react-native-reanimated/plugin'
+    ],
+  };
+};
